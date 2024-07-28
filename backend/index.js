f367bec72e0c2b670f7d58f2b80b62f8cd46ae34
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-07-28 18:43:37 +0530

    sssss

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,15 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Database connected successfully"))
   .catch(error => console.error("Error connecting to database:", error));
 
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname,'/mern-portfolio/dist')))
-  app.get('*', (req, res)=>{
-     res.sendFile(path.join(__dirname, 'mern-portfolio','dist','index.html' ))
-  });
-
+ 
 
 // Route middleware
 app.use('/admin', adminRoutes);
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname,'/mern-portfolio/dist')))
+app.get('*', (req, res)=>{
+   res.sendFile(path.join(__dirname, 'mern-portfolio','dist','index.html' ))
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
