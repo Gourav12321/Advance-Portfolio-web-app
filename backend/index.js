231ae8e75d06bf3e4282b36629196358aea799d0
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-07-28 15:33:28 +0530

    Advance Portfolio Web App

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+// server.js
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import adminRoutes from './Routes/Routes.js';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+dotenv.config();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Database connected successfully"))
+  .catch(error => console.error("Error connecting to database:", error));
+
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname,'/mern-portfolio/dist')))
+  app.get('*', (req, res)=>{
+     res.sendFile(path.join(__dirname, 'mern-portfolio','dist','index.html' ))
+  });
+
+
+// Route middleware
+app.use('/admin', adminRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
